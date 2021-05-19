cae1ad7b653d953d82f121c892fc723552066f27
stefanobelfiore <stefanobelfiore@yahoo.com>
2021-05-19 17:26:46 +0000

    terminado

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Tasks
 #from models import Person
 
 app = Flask(__name__)
@@ -31,15 +31,28 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_all():
+        all_users = User.get_all()
+
+        return jsonify(all_users), 200
+
+@app.route('/user/<email>', methods=['GET'])
+def get_users_by_email(email):
+    user = User.get_by_email(email)
 
     response_body = {
         "msg": "Hello, this is your GET /user response "
     }
 
-    return jsonify(response_body), 200
+    return jsonify(user), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
+
+
+def get_user():
+    pass
+    
+
--- src/models.py
+++ src/models.py
@@ -1,19 +1,55 @@
 from flask_sqlalchemy import SQLAlchemy
-
+# from import get_user 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
+
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    email = db.Column(db.String(50), unique = True)
+    password = db.Column(db.String(20))
+    is_active = db.Column(db.Boolean(), unique = False)
+ 
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f'User {self.email}, {self.id}'
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+    def get_all():
+        users = User.query.all()
+        print(users)
+        users_dict = list(map(lambda x: x.serialize(), users ))
+        print(users_dict)
+        return users_dict
+
+    def get_by_email(email):
+        user = User.query.filter_by(email=email)
+        user_dict = list(map(lambda x: x.serialize(), user))
+        print(user)
+        return user_dict
+
+class Tasks(db.Model):
+    __tablename__ = 'tasks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(500))
+    done = db.Column(db.Boolean, default = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    task = db.relationship(User, lazy=True)
+
+    def __repr__(self):
+        return f'Task {self.description}, {self.id}'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "description": self.description,
+            "done": self.done
             # do not serialize the password, its a security breach
         }
